f5f1bc0dfe49b6c1467772b74ed82ab54cf04cce
Asparageese <60201247+Asparageese@users.noreply.github.com>
2022-09-18 22:53:03 +0100

    0

--- Models/BS_ID/ll_bs.py
+++ Models/BS_ID/ll_bs.py
@@ -1,18 +1,32 @@
 import tensorflow as tf 
 import numpy as np
 
+sample = 1000
+
+y = []
+x = []
+tile = np.arange(0,10,1)
+for i in range(sample):
+	x.append(tile)
+	y.append(np.random.rand(10))
+
+def loss(target_y, predicted_y):
+  return tf.reduce_mean(tf.square(target_y - predicted_y))
+
 class linear(tf.Module):
-	def __init__(self, value):
-		k1 = tf.Variable(value)
-		k2 = tf.Variable([])
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+		self.w = tf.Variable(tf.random.uniform([10]),name='w')
+		self.b = tf.Variable(tf.random.uniform([10]),name='b')
 
-	@tf.function
-	def multiply(self, x):
-		return (x * self.k1) + k2
+	def __call__(self,x):
+		return self.w * x + self.b
 
-test = linear(2)
-print(test)
+test = linear()
 
+for i in range(sample):
+	s = y[i]
+	with tf.GradientTape() as tape:
+		y_loss = loss(y,test(x[i]))
 
-
-
+	grads = tape.gradient(y_loss,test.variables)
